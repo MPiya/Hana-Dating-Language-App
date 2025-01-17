9fc14cb05dd883de207139675f9e038bd9cf6c0c
Christopher <71850259+MPiya@users.noreply.github.com>
2025-01-17 05:12:06 +0100

    Fix program.cs

--- Hana/Program.cs
+++ Hana/Program.cs
@@ -1,34 +1,22 @@
- using Hana.Controllers;
-
+using Hana.Controllers;
 using Hana.hubs;
 using Microsoft.OpenApi.Models;
-
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Hana.Models;
 using Hana.Hana.Database.Data;
 
-
 var builder = WebApplication.CreateBuilder(args);
 
-
 // Retrieve the connection string from app settings
-var connectionString = builder.Configuration.GetConnectionString("HanaContextConnection") ?? throw new InvalidOperationException("Connection string 'HanaContextConnection' not found.");
+var connectionString = builder.Configuration.GetConnectionString("HanaContextConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 
+// Add HanaContext to the dependency injection container
 builder.Services.AddDbContext<HanaContext>(options => options.UseSqlServer(connectionString));
 
-builder.Services.AddDefaultIdentity<UserProfile>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<HanaContext>();
-
-//// Add HanaContext to the dependency injection container
-//builder.Services.AddDbContext<HanaContext>(options => options.UseSqlServer(connectionString));
-
-
-//builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<HanaContext>();
-
-//// remove RequireConfirmedAccount so the user doesnt need to confirm account. 
-//builder.Services.AddDefaultIdentity<IdentityUser>().AddEntityFrameworkStores<HanaContext>();
-
-
+// Configure Identity
+builder.Services.AddDefaultIdentity<UserIdentity>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<HanaContext>();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
@@ -41,7 +29,6 @@ if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-  
 }
 
 app.UseHttpsRedirection();
@@ -49,14 +36,16 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
+
 //signalR chatHub
 app.MapHub<ChatHub>("/chatHub");
 
-// use this to map with Identiy framework Razor pages
+// use this to map with Identity framework Razor pages
 app.MapRazorPages();
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=index}");
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
